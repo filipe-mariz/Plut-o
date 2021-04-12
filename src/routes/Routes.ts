71147ae5b0965b14abcce41a849d2dae61a593ad
filipe-mariz <57136731+filipe-mariz@users.noreply.github.com>
2021-04-12 12:53:12 -0300

    feat: routes registed

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -4,5 +4,8 @@ import Service from '../app/controller/index'
 const routes = Router();
 
 routes.post('/', Service.index);
+routes.get('/view', Service.view);
+routes.get('/view/:id', Service.viewOne);
+routes.delete('/delete/:id', Service.destroy);
 
 export default routes
