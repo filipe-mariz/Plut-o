f34059e71e54c4a49f8f10e8be2137d19297ba04
filipe-mariz <57136731+filipe-mariz@users.noreply.github.com>
2021-04-12 13:25:06 -0300

    chore: register of the new routes

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
-import Service from '../app/controller/crud'
+import Service from '../app/controller/crud';
+import Login from '../app/controller/login';
 
 const routes = Router();
 
@@ -7,5 +8,8 @@ routes.post('/', Service.index);
 routes.get('/view', Service.view);
 routes.get('/view/:id', Service.viewOne);
 routes.delete('/delete/:id', Service.destroy);
+
+routes.post('/user', Login.index);
+routes.post('/login', Login.login)
 
 export default routes
