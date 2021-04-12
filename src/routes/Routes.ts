77a4c40fe40c7dc9b7f3f4eee672429175a4ec3d
filipe-mariz <57136731+filipe-mariz@users.noreply.github.com>
2021-04-12 13:29:43 -0300

    chore: regiter of the middleware in the routes

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -1,15 +1,16 @@
 import { Router } from 'express';
 import Service from '../app/controller/crud';
 import Login from '../app/controller/login';
+import authMiddleware from '../app/middleware/authMiddleware';
 
 const routes = Router();
 
-routes.post('/', Service.index);
-routes.get('/view', Service.view);
-routes.get('/view/:id', Service.viewOne);
-routes.delete('/delete/:id', Service.destroy);
-
 routes.post('/user', Login.index);
-routes.post('/login', Login.login)
+routes.post('/login', Login.login);
+
+routes.post('/', Service.index);
+routes.get('/view', authMiddleware, Service.view);
+routes.get('/view/:id', authMiddleware, Service.viewOne);
+routes.delete('/delete/:id', authMiddleware, Service.destroy);
 
 export default routes
